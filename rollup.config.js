50a3685edc0b5df8d176b974738b8dbb9c87cd75
Jove Jonovski <jovejonovski@gmail.com>
2022-03-22 08:54:05 +0100

    CommonJS support

--- rollup.config.js
+++ rollup.config.js
@@ -6,7 +6,10 @@ import pkg from "./package.json";
 
 export default defineConfig({
   input: "./src/index.ts",
-  output: [{ file: pkg.main, format: "esm" }],
+  output: [
+    { file: "./dist/index.js", format: "esm" },
+    { file: "./dist/index.cjs", format: "cjs" }
+  ],
   external: [...builtinModules, ...Object.keys(pkg.dependencies ?? {}), ...Object.keys(pkg.devDependencies ?? {})],
   plugins: [typescript({ tsconfig: "./tsconfig.json" }), commonjs({ extensions: [".ts", ".js"] })]
 });
